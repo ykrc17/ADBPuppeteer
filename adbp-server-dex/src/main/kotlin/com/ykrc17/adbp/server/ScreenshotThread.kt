606276fc70efd201d469915fa34193b1b45c4cb6
ykrc17 <ykrc17@163.com>
2018-09-20 15:48:51 +0800

    使用单独的线程进行截图

---
+++ adbp-server-dex/src/main/kotlin/com/ykrc17/adbp/server/ScreenshotThread.kt
@@ -0,0 +1,19 @@
+package com.ykrc17.adbp.server
+
+import android.graphics.Bitmap
+import android.view.SurfaceControl
+import java.util.concurrent.LinkedTransferQueue
+
+object ScreenshotThread : Thread() {
+    val queue = LinkedTransferQueue<Bitmap>()
+
+    override fun run() {
+        super.run()
+        while (true) {
+            val time = System.currentTimeMillis()
+            val bitmap = SurfaceControl.screenshot(360, 640)
+            println("screenshot: " + (System.currentTimeMillis() - time))
+            queue.transfer(bitmap)
+        }
+    }
+}
